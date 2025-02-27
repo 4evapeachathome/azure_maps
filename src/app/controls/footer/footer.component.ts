24d31b5b52b659ef2864e395cd526c16ac8a3b0e
MugileeshwaranS <mugileeshwarans@bgca.org>
2025-02-27 17:19:20 +0530

    Added navigation for router link in footer component

--- src/app/controls/footer/footer.component.ts
+++ src/app/controls/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -7,7 +8,7 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
   standalone: true,
-    imports: [CommonModule, IonicModule]
+    imports: [CommonModule, IonicModule, RouterModule]
 })
 export class FooterComponent  implements OnInit {
 
