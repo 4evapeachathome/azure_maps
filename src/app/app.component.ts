12ae9434ecd2f0f06f87912941cecb161671f088
senthilkumar <senthil@ilink-systems.com>
2025-03-14 13:49:04 +0530

    menu v2 updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { MenuComponent } from './components/menu/menu.component';
 import { FooterComponent } from './controls/footer/footer.component';
+import { HeaderComponent } from "./controls/header/header.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [IonicModule, MenuComponent]
+  imports: [IonicModule, MenuComponent, FooterComponent, HeaderComponent]
 })
 export class AppComponent {
   constructor() {}
