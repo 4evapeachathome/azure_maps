2dd9e1195a284762094c94f714d58cd9bc406204
MugileeshwaranS <mugileeshwarans@bgca.org>
2025-03-18 14:14:12 +0530

    Dynmically set heights

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { MenuComponent } from './components/menu/menu.component';
 import { FooterComponent } from './controls/footer/footer.component';
 import { HeaderComponent } from "./controls/header/header.component";
+import { HeightService } from 'src/shared/height.service';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,16 @@ import { HeaderComponent } from "./controls/header/header.component";
   standalone: true,
   imports: [IonicModule, MenuComponent, FooterComponent, HeaderComponent]
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements AfterViewInit {
+  @ViewChild('divScroll', { static: false }) divScroll!: ElementRef;
+
+  constructor(private renderer: Renderer2, private heightService: HeightService) {}
+
+  ngAfterViewInit() {
+    this.heightService.currentHeight.subscribe((height) => {
+      if (this.divScroll) {
+        this.renderer.setStyle(this.divScroll.nativeElement, 'min-height', `${height}px`);
+      }
+    });
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { MenuComponent } from '../components/menu/menu.component';
 import { APIEndpoints } from 'src/shared/endpoints';
+import { HeightService } from 'src/shared/height.service';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,15 @@ import { APIEndpoints } from 'src/shared/endpoints';
   standalone: false,
   // imports: [IonicModule, MenuComponent]
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
   sliderEndpoint:string = APIEndpoints.sliderapi;
-  constructor() {}
+  @ViewChild('ionGrid', { static: false }) ionGrid!: ElementRef;
+  constructor(private heightService: HeightService) {}
+
+  ngAfterViewInit() {
+    if (this.ionGrid) {
+      const gridHeight = this.ionGrid.nativeElement.offsetHeight;
+      this.heightService.updateHeight(gridHeight); // Send height to service
+    }
+  }
 }
