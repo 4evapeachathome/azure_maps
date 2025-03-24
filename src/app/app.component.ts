08e0ebb8149b051d08e3aa538bb28bcb852dcd4d
MugileeshwaranS <mugileeshwarans@bgca.org>
2025-03-24 14:40:09 +0530

    checekd property handled for mobile and nonmmoile device

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,25 @@
 import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, Platform } from '@ionic/angular';
 import { MenuComponent } from './components/menu/menu.component';
 import { FooterComponent } from './controls/footer/footer.component';
 import { HeaderComponent } from "./controls/header/header.component";
 import { HeightService } from 'src/shared/height.service';
 import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [IonicModule, MenuComponent, FooterComponent, HeaderComponent]
+  imports: [IonicModule, MenuComponent, HeaderComponent, CommonModule]
 })
 export class AppComponent implements OnInit,OnDestroy {
-  minHeight: string = '250px';
-  private heightSubscription!: Subscription;
+  isMobile!: boolean;
 
-  constructor(private heightService: HeightService) {}
+  constructor(private platform: Platform) {
+    this.isMobile = this.platform.is('android') || this.platform.is('ios');
+  }
 
   ngOnInit() {
   }
