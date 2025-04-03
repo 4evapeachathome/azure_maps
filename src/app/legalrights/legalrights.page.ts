b435a73fc91bf266665d230acd70b9abece2b504
MugileeshwaranS <mugileeshwarans@bgca.org>
2025-04-03 15:39:00 +0530

    Conflict resolved

--- src/app/legalrights/legalrights.page.ts
+++ src/app/legalrights/legalrights.page.ts
@@ -1,33 +1,3 @@
-<<<<<<< .mine
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-legalrights',
-  templateUrl: './legalrights.page.html',
-  styleUrls: ['./legalrights.page.scss'],
-  standalone: false
-})
-export class LegalrightsPage implements OnInit {
- 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-=======
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -45,15 +15,5 @@ export class LegalrightsPage implements OnInit {
   ngOnInit() {
   }
 
-  onStateClick(state: { id: string; name: string; path: string }) {
-   // debugger;
-    this.selectedState = state; // Update selected state for breadcrumb
-    console.log('Selected state:', state.name);
-  }
 
-  resetSelectedState() {
-   // debugger;
-    this.selectedState = null; // Reset state when breadcrumb is clicked
-  }
 }
->>>>>>> .theirs
