f560c3a19ca82aded5157b9def5246e857b3a2a0
MugileeshwaranS <mugileeshwarans@bgca.org>
2025-04-24 18:01:28 +0530

    Bug fix for types of abuse page

--- src/app/typesofabuses/typesofabuses.page.ts
+++ src/app/typesofabuses/typesofabuses.page.ts
@@ -27,6 +27,7 @@ export class TypesofabusesPage implements OnInit {
   }
 
   scrollToSection(sectionId: string) {
+    this.activeSection = sectionId;
     const element = document.getElementById(sectionId);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth', block: 'start' });
