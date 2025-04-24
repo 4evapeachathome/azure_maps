aac4c9dcc5ede451daca54621b0d00f04e16cec5
MugileeshwaranS <mugileeshwarans@bgca.org>
2025-04-24 20:25:16 +0530

    List logic added in the types-of abuse card

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: true,
-  apiHost:'https://peaceathome.loca.lt',
+  apiHost:'https://nsfdvapi-facubcdsffcpdmgj.eastus2-01.azurewebsites.net',
   //apiHost:'http://localhost:1337',
   secretKey:'0244387ac5f95d2f5ae4b5e560e4c617f4b59857378d6579041229fdbb44dee9',
   apitoken:'9d689662d625cea1c398e6cad3cf0e7387be9d29af8c6802fa837a034e38dd4b7dbcffd3afe7ba05903122e920bb1901570cd6b86c5004fd0e6f5c78837239797ffd42d4122299c1c3c6987c508c11c7a46ac0390223a9de7e5496d351d318dbe8a724dd383d42a0d859ab0a4b7e28816663e997c056924dc67ba5f32456b7d3'
