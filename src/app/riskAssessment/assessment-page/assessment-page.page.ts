661befb215e12b5dd125fc4c98103de762015eb5
MugileeshwaranS <mugileeshwarans@bgca.org>
2025-05-21 12:12:22 +0530

    Loader added for risk assessment pages

--- src/app/riskAssessment/assessment-page/assessment-page.page.ts
+++ src/app/riskAssessment/assessment-page/assessment-page.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-assessment-page',
@@ -6,11 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assessment-page.page.scss'],
   standalone: false,
 })
-export class AssessmentPagePage implements OnInit {
+export class AssessmentPagePage implements OnInit,AfterViewInit {
+  loading: HTMLIonLoadingElement | null = null;
 
-  constructor() { }
+  constructor(private loadingController: LoadingController) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    // Only show loader if not pre-rendered
+    await this.showLoader();
   }
 
+  async ngAfterViewInit() {
+    const idleCallback = window['requestIdleCallback'] || function (cb: any) {
+      setTimeout(cb, 1000);
+    };
+
+    idleCallback(() => {
+      setTimeout(() => {
+        this.hideLoader();
+      }, 500);
+    });
+  }
+
+  async showLoader() {
+    this.loading = await this.loadingController.create({
+      message: 'Loading...',
+      spinner: 'crescent',
+      backdropDismiss: false,
+    });
+    await this.loading.present();
+
+    // Force dismiss after 10 seconds just in case
+    setTimeout(() => {
+      this.hideLoader();
+    }, 5000);
+  }
+
+  async hideLoader() {
+    if (this.loading) {
+      try {
+        await this.loading.dismiss();
+      } catch (e) {
+        console.warn('Loader already dismissed or not yet created');
+      }
+      this.loading = null;
+    }
+  }
 }
