32a360b3f20a006efc901adc981227bff65068b6
senthilkumar <senthil@ilink-systems.com>
2025-05-29 19:49:21 +0530

    updated tool tips

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -267,9 +267,9 @@ collapseAllSections(): void {
 
   getTooltip(name: string): string | null {
     if (name === '/quiz') {
-      return 'Quiz for Healthy and Unhealthy Relationship';
+      return 'HUHA Quiz';
     } else if (name === '/sripaa') {
-      return 'Signs of Self-Recognition in Intimate Partner Abuse';
+      return 'SSRIPA Assessment';
     }
     return null;
   }
