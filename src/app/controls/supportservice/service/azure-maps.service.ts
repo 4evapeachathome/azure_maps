eb8875af9c7324c074174f85a8321323137aa38a
MugileeshwaranS <mugileeshwarans@bgca.org>
2025-07-21 12:24:34 +0530

    Get Azure key from the api call

--- src/app/controls/supportservice/service/azure-maps.service.ts
+++ src/app/controls/supportservice/service/azure-maps.service.ts
@@ -1,37 +1,45 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, filter, firstValueFrom, Observable, switchMap, take } from 'rxjs';
+import { MenuService } from 'src/shared/menu.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AzureMapsService {
   private baseUrl = 'https://atlas.microsoft.com/search/address/json';
-  private subscriptionKey = 'BiKCkDXwHMOGze7XpZU6n9P9Zk0FxHzh2yBuNVOL40XZ2kUlXK4zJQQJ99BGACYeBjFYJtvcAAAgAZMPjlq3'; // 🔒 Replace with your key
+  private azureKey: any;
+  private azureKeySubject = new BehaviorSubject<string | null>(null);
+  constructor(private http: HttpClient, private sharedDataService: MenuService) { 
+   this.sharedDataService.dataLoaded$.pipe(
+    filter(ready => ready),
+    take(1)
+  ).subscribe(async () => {
+    const configMap = await firstValueFrom(this.sharedDataService.config$);
+    this.azureKey = configMap['azureMapsKey'];
+    this.azureKeySubject.next(this.azureKey);
+  });
+  }
 
-  constructor(private http: HttpClient) {}
+private waitForKey(): Observable<string> {
+  return this.azureKeySubject.pipe(
+    filter((key): key is string => !!key), 
+    take(1)
+  );
+}
 
 getGeocodeResult(query: string): Observable<any> {
-  const params = new HttpParams()
-    .set('api-version', '1.0')
-    .set('query', query)
-    .set('limit', '1')
-    .set('countrySet', 'US')
-    .set('language', 'en-US')
-    .set('subscription-key', this.subscriptionKey);
-
-  return this.http.get(this.baseUrl, { params });
+  return this.waitForKey().pipe(
+    switchMap(key => {
+      const params = new HttpParams()
+        .set('api-version', '1.0')
+        .set('query', query)
+        .set('limit', '1')
+        .set('countrySet', 'US')
+        .set('language', 'en-US')
+        .set('subscription-key', key);
+      return this.http.get(this.baseUrl, { params });
+    })
+  );
 }
-  getPlacePredictions(query: string): Observable<any> {
-    const params = new HttpParams()
-      .set('api-version', '1.0')
-      .set('typeahead', 'true')
-      .set('query', query)
-      .set('limit', '5')
-      .set('countrySet', 'US')
-      .set('language', 'en-US')
-      .set('subscription-key', this.subscriptionKey);
-
-    return this.http.get(this.baseUrl, { params });
-  }
 }
